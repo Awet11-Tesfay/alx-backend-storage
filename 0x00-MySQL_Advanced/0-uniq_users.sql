e48b625b333048978304592bf499a140abaf2541
Awet11-Tesfay <hailegb2021@gmail.com>
2023-04-06 10:16:25 +0000

    first table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,7 +2,6 @@
 -- requirements (id, email, name)
 CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	emai VARCHAR(255) NOT NULL UNIQUE,
+	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255)
 );
-
