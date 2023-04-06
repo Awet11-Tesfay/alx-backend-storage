42e5384b09fd44b052d6ad0b379e808883acf714
Awet11-Tesfay <hailegb2021@gmail.com>
2023-04-06 19:19:23 +0000

    average weight

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,18 +1,18 @@
 -- sql script that creates stored procedure
--- calculate-weighted-average
+-- calculate weight average
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-     UPDATE users AS U,
-	 (SELECT U.id, SUM(score * weight) / SUM(weight) AS w_avg
-	 FROM users AS U
-	 JOIN corrections as C ON U.id=C.user.id
-	 JOIN projects AS ON C.project_id=P.id
-	 GROUP BY U.id)
-     AS WA
-     SET U.average_score = WA.w_avg
-     WHERE U.id=WA.id;
+    UPDATE users AS U,
+        (SELECT U.id, SUM(score * weight) / SUM(weight) AS w_avg
+        FROM users AS U
+        JOIN corrections as C ON U.id=C.user_id
+        JOIN projects AS P ON C.project_id=P.id
+        GROUP BY U.id)
+    AS WA
+    SET U.average_score = WA.w_avg
+    WHERE U.id=WA.id;
 END
 $$
 DELIMITER ;
