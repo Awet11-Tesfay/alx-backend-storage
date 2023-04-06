087dd761cc50f3678ef35739b74dc829dfbe04aa
Awet11-Tesfay <hailegb2021@gmail.com>
2023-04-06 14:48:55 +0000

    best band

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- sql script that ranks country orgins of bands, ordered 
 -- by the number of (non-unique) fans
-SELECT DISTINCT as origin, SUM(fans) as nb_fans from metal_bands
+SELECT DISTINCT  AS origin, SUM(fans) AS nb_fans from metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
