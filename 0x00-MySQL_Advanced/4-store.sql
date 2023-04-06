54e0531abc45378a4cdef85d4d01d50368f69605
Awet11-Tesfay <hailegb2021@gmail.com>
2023-04-06 19:41:25 +0000

    buy

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,9 +1,13 @@
 -- sql script that creates a trigger
 -- decreases the quantity of an item
-CREATE TRIGGER  decrease_quantity
-AFTER INSERT ON 
-orders FOR EACH ROW
-START 
-UPDATE items
-SET quantity = quantity - NEW.number
-WHERE name = New.item_name;
+DROP TRIGGER IF EXISTS down_quantity;
+DELIMITER $$
+CREATE TRIGGER reduce_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+        SET quantity = quantity - NEW.number
+        WHERE name = NEW.item_name;
+END $$
+DELIMITER ;
