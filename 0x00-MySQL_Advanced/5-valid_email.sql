8ea8335ac86e13c65b8e3d2146491c87748727b4
Awet11-Tesfay <hailegb2021@gmail.com>
2023-04-06 18:26:11 +0000

    email validation

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,6 +1,6 @@
 -- a trigger that resets the attribute valid_email only when the emai has been changed
 -- email validation
-CREATE TRIGGER email_int BEFORE UPDATE ON users
+CREATE TRIGGER email BEFORE UPDATE ON users
 FOR EACH ROW
 	IF NEW.email <> OLD.email
 	THEN
