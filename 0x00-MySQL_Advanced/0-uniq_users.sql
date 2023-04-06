c63e7267ad14b5f4504fc223e10a7e324942c444
Awet11-Tesfay <hailegb2021@gmail.com>
2023-04-06 10:19:49 +0300

    first table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- Sql script that creates a table "users" with the ff requrements
---- reqirements(id, email, name)
-CREATE TABLE user IF NOT EXISTS(
+-- reqirements(id, email, name)
+CREATE TABLE IF NOT EXISTS users (
     id INT Not NULL AUTO_INCREMENT, PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
