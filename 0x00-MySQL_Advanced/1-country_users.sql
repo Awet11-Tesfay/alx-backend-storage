455e460db83202dc8088828b6525c65b5ede582f
Awet11-Tesfay <hailegb2021@gmail.com>
2023-04-06 10:30:33 +0000

    in and not out

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,1 +1,8 @@
--- 
+-- script that creates a table users
+-- attributes (id ,email, name, country)
+CREATE TABLE IN NOT EXISTS users (
+       id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+       email VARCHAR(255) NOT NULL UNIQUE,
+       name VARCHAR(255),
+       country ENUM(`US`, `CO`, `TN`) NOT NULL DEFAULT `US`
+);
