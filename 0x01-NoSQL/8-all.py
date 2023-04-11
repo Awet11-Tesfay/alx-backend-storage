5255bcb11fca051d0bb190889fe5dd98f6052d7c
Awet11-Tesfay <hailegb2021@gmail.com>
2023-04-11 16:52:01 +0300

    list all cocument

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,8 +1,9 @@
-#!/user/bin/env python3
+#!/usr/bin/env python3
 """ list of documents in a collection """
 
+
 def list_all(mongo_collection):
-    """ if there is not document in the collection it will return an empty list"""
+    """ if there is not document in the collection return empty list"""
     mongo = mongo_collection.find().count()
     if mongo_collection.count() == 0:
         return []
