749008fcab74d427a275c0b0a1fa754f2d875670
Awet11-Tesfay <hailegb2021@gmail.com>
2023-04-11 17:11:08 +0300

    update many

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" using to update or change the elements based on the name """
+
+
+def update_topics(mongo_collection, name, topics):
+    db = {name: "name"}
+    values = {"$set": {topics: "topics"}}
+    mongo_collection.updateMany(db, values)
