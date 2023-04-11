976de696eb4506af663b2a095cfb34df131ebd9b
Awet11-Tesfay <hailegb2021@gmail.com>
2023-04-11 18:41:57 +0300

    changes

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -4,6 +4,6 @@
 
 def update_topics(mongo_collection, name, topics):
     """ changes all topics of a school based on name """
-    query = {"name": name}
-    new_values = {"$set": {"topics": topics}}
-    mongo_collection.update_many(query, new_values)
+    db = {"name": name}
+    values = {"$set": {"topics": topics}}
+    mongo_collection.update_many(db, values)
